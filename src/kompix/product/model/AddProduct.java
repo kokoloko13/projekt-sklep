2a977adf6cb31263795d2f49f148808b0802affa
baterss <batrix4@gmail.com>
2020-05-10 21:19:45 +0200

    model addproduct

---
+++ src/kompix/product/model/AddProduct.java
@@ -0,0 +1,85 @@
+package kompix.product.model;
+
+public class AddProduct {
+    private String name;
+    private int amount;
+    private float price;
+    private String describe;
+    private String category;
+    private String photo1;
+    private String photo2;
+    private String photo3;
+    private String photo4;
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public int getAmount(){
+        return amount;
+    }
+
+    public void setAmount(int amount){
+        this.amount=amount;
+    }
+
+    public float getPrice(){
+        return price;
+    }
+
+    public void setPrice(float price){
+        this.price=price;
+    }
+
+    public String getDescribe(){
+        return describe;
+    }
+
+    public void setDescribe(String describe){
+        this.describe=describe;
+    }
+
+    public String getCategory(){
+        return category;
+    }
+
+    public void setCategory(String category){
+        this.category=category;
+    }
+
+    public String getPhoto1(){
+        return photo1;
+    }
+
+    public void setPhoto1(String photo1){
+        this.photo1=photo1;
+    }
+
+    public String getPhoto2(){
+        return photo2;
+    }
+
+    public void setPhoto2(String photo1){
+        this.photo2=photo2;
+    }
+    public String getPhoto3(){
+        return photo3;
+    }
+
+    public void setPhoto3(String photo1){
+        this.photo3=photo3;
+    }
+
+    public String getPhoto4(){
+        return photo4;
+    }
+
+    public void setPhoto4(String photo1){
+        this.photo4=photo4;
+    }
+
+}
